afc45450fd3c205f6df69e51c1be6384989b3d8f
Seba Alarcon <salarcona@usm.cl>
2026-06-12 20:44:59 -0400

    pagina de categoras v0.1

--- appnexo/views.py
+++ appnexo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Producto, Categoria
 
-# Create your views here.
+def lista_productos(request):
+    productos = Producto.objects.all()
+    categorias = Categoria.objects.all()
+    return render(request, 'appnexo/categorias.html', {
+        'productos': productos,
+        'categorias': categorias,
+    })
